a2a3f2089b6ed329b3683202bd3b91d9235a32a8
RenanBernardi <renandigisat@gmail.com>
2024-04-25 09:46:25 -0300

    25-04

--- mobile_nfse/urls.py
+++ mobile_nfse/urls.py
@@ -1,11 +1,15 @@
 # urls.py do seu aplicativo
 from django.urls import path
 from . import views
+from .views import login_view
+from .views import index_view
+from .views import emissao_nfse
+from .views import endereco_servidor
 
 urlpatterns = [
-    path('', views.index_view, name='index'),  # Redireciona a raiz para a tela de login
-    path('login/', views.login_view, name='login'),
-    path('emissao_nfse/', views.emissao_nfse, name='emissao_nfse'),
-    path('endereco_servidor/', views.endereco_servidor, name='endereco_servidor'),
+    path('', index_view, name='index'),  # Redireciona a raiz para a tela de login
+    path('login/', login_view, name='login'),
+    path('emissao_nfse/', emissao_nfse, name='emissao_nfse'),
+    path('endereco_servidor/', endereco_servidor, name='endereco_servidor'),
 ]
 
