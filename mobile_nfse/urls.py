368069720744c635713f36b8c93359a42a3aa4ca
RenanBernardi <renandigisat@gmail.com>
2024-05-03 13:40:57 -0300

    03-05

--- mobile_nfse/urls.py
+++ mobile_nfse/urls.py
@@ -5,10 +5,12 @@ from .views import login_view
 from .views import index_view
 from .views import emissao_nfse
 from .views import endereco_servidor
+from .views import totalizacao
 
 urlpatterns = [
     path('', index_view, name='index'),  # Redireciona a raiz para a tela de login
     path('login/', login_view, name='login'),
     path('emissao_nfse/', emissao_nfse, name='emissao_nfse'),
     path('endereco_servidor/', endereco_servidor, name='endereco_servidor'),
+    path('totalizacao/', totalizacao, name='totalizacao'),
 ]
--- mobile_nfse/views.py
+++ mobile_nfse/views.py
@@ -1,16 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.template.loader import render_to_string
 
+html_content = render_to_string('login.html')
+html_content = render_to_string('index.html')
+html_content = render_to_string('endereco_servidor.html')
+html_content= render_to_string('emissao_nfse.html')
+html_content = render_to_string('totalizacao.html')
 
 def index_view(request):
     return render(request, 'index.html')
 
 def login_view(request):
     if request.method == 'POST':
-            return redirect('emissao_nfse')
+        return redirect('emissao_nfse')
     else:
-            return render(request, 'login.html')
+        return render(request, 'login.html')
 
 
 def emissao_nfse(request):
@@ -21,7 +27,30 @@ def emissao_nfse(request):
 
 
 def endereco_servidor(request):
-     if request.method == 'POST':
-          return redirect('endereco_servidor')
-     else:
-          return render(request, 'endereco_servidor.html')
+    if request.method == 'POST':
+        return redirect('endereco_servidor')
+    else:
+        return render(request, 'endereco_servidor.html')
+
+def totalizacao(request):
+    if request.method == 'POST':
+        return redirect('totalizacao')
+    else:
+        valor_servico = float(request.POST.get('valor_servico', 0))
+        desconto = float(request.POST.get('desconto', 0))
+        tipo_desconto = request.POST.get('tipo_desconto')
+
+        valor_desconto = None
+        if tipo_desconto == 'porcentagem':
+            valor_desconto = (valor_servico * desconto) / 100
+        elif tipo_desconto == 'valor':
+            valor_desconto = desconto
+
+        if valor_desconto is not None:
+            total = valor_servico - valor_desconto
+        else:
+            total = valor_servico
+
+        return render(request, 'totalizacao.html', {'total': total})
+    
+    
